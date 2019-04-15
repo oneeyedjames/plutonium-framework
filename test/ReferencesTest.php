22b3d70354dd76d5c9e65ec4bfd0f1b8dc96b38a
J Andrew Scott <jascott@programmer.net>
2019-04-15 12:39:20 -0400

    removed non-framework tests from phpunit

--- test/ReferencesTest.php
+++ test/ReferencesTest.php
@@ -1,151 +1,191 @@
-<?php
+f<?php
 
 use PHPUnit\Framework\TestCase;
 
 class ReferencesTest extends TestCase {
+	public function setUp() {
+		$this->var = 0;
+		$this->obj = (object) ['var' => 0];
+	}
+
 	public function testPassByValue() {
-		$a = (object) ['var' => 0];
-		$b = $a;
+		$var = $this->var;
+		$obj = $this->obj;
 
-		$this->passByVal($a);
+		$this->passScalarByValue($var);
+		$this->passObjectByValue($obj);
 
-		$this->assertEquals($a->var, 1);
-		$this->assertEquals($b->var, 1);
+		$this->assertEquals($var, 0);
+		$this->assertEquals($obj->var, 1);
 	}
 
-	public function testPassByValueNull() {
-		$a = (object) ['var' => 0];
-		$b = $a;
+	// public function testPassByValueNull() {
+	// 	$a = (object) ['var' => 0];
+	// 	$b = $a;
+	//
+	// 	$this->passObjectByValue($a);
+	//
+	// 	$a = null;
+	//
+	// 	$this->assertNull($a);
+	// 	$this->assertEquals($b->var, 1);
+	// }
 
-		$this->passByVal($a);
+	// public function testPassByValueUndefined() {
+	// 	$a = (object) ['var' => 0];
+	// 	$b = $a;
+	//
+	// 	$this->passObjectByValue($a);
+	//
+	// 	unset($a);
+	//
+	// 	$this->assertNull(@$a);
+	// 	$this->assertEquals($b->var, 1);
+	// }
 
-		$a = null;
-
-		$this->assertNull($a);
-		$this->assertEquals($b->var, 1);
-	}
-
-	public function testPassByValueUndefined() {
-		$a = (object) ['var' => 0];
-		$b = $a;
-
-		$this->passByVal($a);
-
-		unset($a);
-
-		$this->assertNull(@$a);
-		$this->assertEquals($b->var, 1);
-	}
-
-	public function testPassByValueReassigned() {
-		$a = (object) ['var' => 0];
-		$b = $a;
-
-		$this->passByVal($a);
-
-		$a = (object) ['var' => 5];
-
-		$this->assertEquals($a->var, 5);
-		$this->assertEquals($b->var, 1);
-	}
+	// public function testPassByValueReassigned() {
+	// 	$a = (object) ['var' => 0];
+	// 	$b = $a;
+	//
+	// 	$this->passObjectByValue($a);
+	//
+	// 	$a = (object) ['var' => 5];
+	//
+	// 	$this->assertEquals($a->var, 5);
+	// 	$this->assertEquals($b->var, 1);
+	// }
 
 	public function testPassByReference() {
-		$a = (object) ['var' => 0];
-		$b =& $a;
+		$var = $this->var;
+		$obj = $this->obj;
 
-		$this->passByRef($a);
+		$this->passScalarByReference($var);
+		$this->passObjectByReference($obj);
 
-		$this->assertEquals($a->var, 1);
-		$this->assertEquals($b->var, 1);
+		$this->assertEquals($var, 1);
+		$this->assertEquals($obj->var, 1);
 	}
 
-	public function testPassByReferenceNull() {
-		$a = (object) ['var' => 0];
-		$b =& $a;
+	// public function testPassByReferenceNull() {
+	// 	$a = (object) ['var' => 0];
+	// 	$b =& $a;
+	//
+	// 	$this->passObjectByReference($a);
+	//
+	// 	$a = null;
+	//
+	// 	$this->assertNull($a);
+	// 	$this->assertNull($b);
+	// }
 
-		$this->passByRef($a);
+	// public function testPassByReferenceUndefined() {
+	// 	$a = (object) ['var' => 0];
+	// 	$b =& $a;
+	//
+	// 	$this->passObjectByReference($a);
+	//
+	// 	unset($a);
+	//
+	// 	$this->assertNull(@$a);
+	// 	$this->assertEquals($b->var, 1);
+	// }
 
-		$a = null;
-
-		$this->assertNull($a);
-		$this->assertNull($b);
-	}
-
-	public function testPassByReferenceUndefined() {
-		$a = (object) ['var' => 0];
-		$b =& $a;
-
-		$this->passByRef($a);
-
-		unset($a);
-
-		$this->assertNull(@$a);
-		$this->assertEquals($b->var, 1);
-	}
-
-	public function testPassByReferenceReassigned() {
-		$a = (object) ['var' => 0];
-		$b =& $a;
-
-		$this->passByRef($a);
-
-		$a = (object) ['var' => 5];
-
-		$this->assertEquals($a->var, 5);
-		$this->assertEquals($b->var, 5);
-	}
+	// public function testPassByReferenceReassigned() {
+	// 	$a = (object) ['var' => 0];
+	// 	$b =& $a;
+	//
+	// 	$this->passObjectByReference($a);
+	//
+	// 	$a = (object) ['var' => 5];
+	//
+	// 	$this->assertEquals($a->var, 5);
+	// 	$this->assertEquals($b->var, 5);
+	// }
 
 	public function testReturnByValue() {
-		$this->var = (object) ['var' => 0];
+		$var = $this->returnScalarByValue();
+		$obj = $this->returnObjectByValue();
 
-		$val = $this->returnByVal();
+		$this->assertEquals(0, $var);
+		$this->assertEquals(0, $this->var);
+		$this->assertEquals(0, $obj->var);
+		$this->assertEquals(0, $this->obj->var);
 
-		$this->assertEquals(0, $val->var);
-		$this->assertEquals(0, $this->var->var);
+		$this->var++;
+		$this->obj->var++;
 
-		$this->var->var++;
+		// different values because scalars are returned by value
+		$this->assertEquals(0, $var);
+		$this->assertEquals(1, $this->var);
 
-		$this->assertEquals(1, $val->var);
-		$this->assertEquals(1, $this->var->var);
+		// same values because objects are always returned by reference
+		$this->assertEquals(1, $obj->var);
+		$this->assertEquals(1, $this->obj->var);
 
-		$val->var++;
+		$var++;
+		$obj->var++;
 
-		$this->assertEquals(2, $val->var);
-		$this->assertEquals(2, $this->var->var);
+		$this->assertEquals(1, $var);
+		$this->assertEquals(1, $this->var);
+		$this->assertEquals(2, $obj->var);
+		$this->assertEquals(2, $this->obj->var);
 	}
 
 	public function testReturnByReference() {
-		$this->var = (object) ['var' => 0];
+		$var =& $this->returnScalarByReference();
+		$obj =& $this->returnObjectByReference();
 
-		$ref = $this->returnByRef();
+		$this->assertEquals(0, $var);
+		$this->assertEquals(0, $this->var);
+		$this->assertEquals(0, $obj->var);
+		$this->assertEquals(0, $this->obj->var);
 
-		$this->assertEquals(0, $ref->var);
-		$this->assertEquals(0, $this->var->var);
+		$this->var++;
+		$this->obj->var++;
 
-		$this->var->var++;
+		$this->assertEquals(1, $var);
+		$this->assertEquals(1, $this->var);
+		$this->assertEquals(1, $obj->var);
+		$this->assertEquals(1, $this->obj->var);
 
-		$this->assertEquals(1, $ref->var);
-		$this->assertEquals(1, $this->var->var);
+		$var++;
+		$obj->var++;
 
-		$ref->var++;
-
-		$this->assertEquals(2, $ref->var);
-		$this->assertEquals(2, $this->var->var);
+		$this->assertEquals(2, $var);
+		$this->assertEquals(2, $this->var);
+		$this->assertEquals(2, $obj->var);
+		$this->assertEquals(2, $this->obj->var);
 	}
 
-	public function passByVal($obj) {
+	public function passScalarByValue($var) {
+		$var++;
+	}
+
+	public function passObjectByValue($obj) {
 		$obj->var++;
 	}
 
-	public function passByRef(&$obj) {
+	public function passScalarByReference(&$var) {
+		$var++;
+	}
+
+	public function passObjectByReference(&$obj) {
 		$obj->var++;
 	}
 
-	public function returnByVal() {
+	public function returnScalarByValue() {
 		return $this->var;
 	}
 
-	public function &returnByRef() {
+	public function returnObjectByValue() {
+		return $this->obj;
+	}
+
+	public function &returnScalarByReference() {
 		return $this->var;
+	}
+
+	public function &returnObjectByReference() {
+		return $this->obj;
 	}
 }
