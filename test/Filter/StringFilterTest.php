d85fe6ccd0e43719907ee93bdf6b6cc9c41ee15f
J Andrew Scott <jascott@programmer.net>
2020-06-24 08:58:43 -0400

    clean up Filter and Utility tests

--- test/Filter/StringFilterTest.php
+++ test/Filter/StringFilterTest.php
@@ -5,37 +5,53 @@ use PHPUnit\Framework\TestCase;
 use Plutonium\Filter\StringFilter;
 
 class StringFilterTest extends TestCase {
-	var $filter;
-
 	public function setUp() {
 		$this->filter = new StringFilter();
 	}
 
+	/*
+	 * Tests that only alpha characters are returned.
+	 */
 	public function testFilterAlpha() {
 		$this->assertNotFalse($this->filter->canHandle('alpha'));
 		$this->assertSame('FooBar', $this->filter->filter('FooBar123@#!', 'alpha'));
 	}
 
+	/*
+	 * Tests that only alphanumeric characters are returned.
+	 */
 	public function testFilterAlnum() {
 		$this->assertNotFalse($this->filter->canHandle('alnum'));
 		$this->assertSame('FooBar123', $this->filter->filter('FooBar123@#!', 'alnum'));
 	}
 
+	/*
+	 * Tests that only decimal digits are returned.
+	 */
 	public function testFilterDigit() {
 		$this->assertNotFalse($this->filter->canHandle('digit'));
 		$this->assertSame('123', $this->filter->filter('FooBar123@#!', 'digit'));
 	}
 
+	/*
+	 * Tests that only hexadecimal hexits are returned.
+	 */
 	public function testFilterHexit() {
 		$this->assertNotFalse($this->filter->canHandle('hexit'));
 		$this->assertSame('FBa123', $this->filter->filter('FooBar123@#!', 'hexit'));
 	}
 
+	/*
+	 * Tests that only lower-case alpha characters are returned.
+	 */
 	public function testFilterLCase() {
 		$this->assertNotFalse($this->filter->canHandle('lcase'));
 		$this->assertSame('ooar', $this->filter->filter('FooBar123@#!', 'lcase'));
 	}
 
+	/*
+	 * Tests that only upper-case alpha characters are returned.
+	 */
 	public function testFilterUCase() {
 		$this->assertNotFalse($this->filter->canHandle('ucase'));
 		$this->assertSame('FB', $this->filter->filter('FooBar123@#!', 'ucase'));
--- test/Utility/AddressTest.php
+++ test/Utility/AddressTest.php
@@ -5,6 +5,9 @@ use PHPUnit\Framework\TestCase;
 use Plutonium\Utility\Address;
 
 class AddressTest extends TestCase {
+	/*
+	 * Tests that an address object can be created from an array.
+	 */
 	public function testAddressArray() {
 		$address = Address::newInstance([127, 0, 0, 1]);
 
@@ -12,6 +15,9 @@ class AddressTest extends TestCase {
 		$this->assertEquals('127.0.0.1', $address->toString());
 	}
 
+	/*
+	 * Tests that an address object can be created from a string.
+	 */
 	public function testAddressString() {
 		$address = Address::newInstance('127.0.0.1');
 
@@ -19,6 +25,9 @@ class AddressTest extends TestCase {
 		$this->assertEquals('127.0.0.1', $address->toString());
 	}
 
+	/*
+	 * Tests that an address object can be created from an integer.
+	 */
 	public function testAddressInt() {
 		$address = Address::newInstance(0x7F000001);
 
@@ -26,6 +35,9 @@ class AddressTest extends TestCase {
 		$this->assertEquals('127.0.0.1', $address->toString());
 	}
 
+	/*
+	 * Tests that short strings are left-padded to proper length.
+	 */
 	public function testAddressTruncatedString() {
 		$address = Address::newInstance('127.0.0');
 
@@ -33,6 +45,9 @@ class AddressTest extends TestCase {
 		$this->assertEquals('0.127.0.0', $address->toString());
 	}
 
+	/*
+	 * Tests that short integers are left-padded to proper length.
+	 */
 	public function testAddressTruncatedInt() {
 		$address = Address::newInstance(0x7F0000);
 
@@ -40,6 +55,9 @@ class AddressTest extends TestCase {
 		$this->assertEquals('0.127.0.0', $address->toString());
 	}
 
+	/*
+	 * Tests that long strings are left-truncated to proper length.
+	 */
 	public function testAddressOverflownString() {
 		$address = Address::newInstance('1.127.0.0.1');
 
@@ -47,6 +65,9 @@ class AddressTest extends TestCase {
 		$this->assertEquals('127.0.0.1', $address->toString());
 	}
 
+	/*
+	 * Tests that long integers are left-truncated to proper length.
+	 */
 	public function testAddressOverflownInt() {
 		$address = Address::newInstance(0x017F000001);
 
