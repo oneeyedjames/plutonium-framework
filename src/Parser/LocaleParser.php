b932e7dbf358b21caf05b23a5a2f0eaabcf0f134
J Andrew Scott <jascott@programmer.net>
2020-07-09 15:09:44 -0400

    Docblocks for Parser namespace

--- src/Parser/LocaleParser.php
+++ src/Parser/LocaleParser.php
@@ -6,6 +6,12 @@
 namespace Plutonium\Parser;
 
 class LocaleParser extends AbstractParser {
+	/**
+	 * Replaces a &lt;pu:trans&gt; tag with a translated string.
+	 * Expected attributes:
+	 *   - phrase: original text for translation
+	 * @param array $args Tag attributes
+	 */
 	public function transTag($args) {
 		return $this->application->locale->localize($args['phrase']);
 	}
--- src/Parser/UtilityParser.php
+++ src/Parser/UtilityParser.php
@@ -6,11 +6,23 @@
 namespace Plutonium\Parser;
 
 class UtilityParser extends AbstractParser {
+	/**
+	 * @param object $application The active Application object
+	 * @param object $args AccessObject containing timezone
+	 */
 	public function __construct($application, $args) {
 		parent::__construct($application);
 		date_default_timezone_set($args->timezone);
 	}
 
+	/**
+	 * Replaces a &lt;pu:date&gt; tag with a formatted date string.
+	 * Expected attributes:
+	 *   - format: a strftime() compatible format string
+	 *   - time: OPTIONAL UNIX timestamp, current time is used if omitted
+	 * @param array $args The tag attributes
+	 * @param string Formatted date
+	 */
 	public function dateTag($args) {
 		$time   = isset($args['time']) ? strtotime($args['time']) : time();
 		$format = isset($args['format']) ? $args['format'] : 'date_format_long';
